15983d9492e78520f462c882b68fa1dd9b23d2d3
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-23 17:10:25 +0600

    Footer section added

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import footer from '../Home/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer style={{
+            background: `url(${footer})`,
+            backgroundSize: 'cover'
+        }} className="p-10">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className='my-10 text-center'>
+                <p>Copyright © 2022 - All right reserved</p>
+            </div>
+        </footer>
     );
 };
 
