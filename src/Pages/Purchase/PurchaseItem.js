483f34cbefa68f5dae99e48ea9ce25f20f0d868d
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-26 07:47:25 +0600

    quantity field validation error set

--- src/Pages/Purchase/PurchaseItem.js
+++ src/Pages/Purchase/PurchaseItem.js
@@ -4,18 +4,33 @@ import { useParams } from "react-router";
 //import { format } from 'date-fns';
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 //import { toast } from 'react-toastify';
 
 const PurchaseItem = () => {
   const { id } = useParams();
   const [purchaseItems, setPurchaseItems] = useState({});
   const [reload, SetIsReload] = useState(true);
+  const [qurantityError, SetQurantityError] = useState(true);
+  //console.log(qurantityError)
   const [user, loading, error] = useAuthState(auth);
+  const { register, formState: { errors }, handleSubmit } = useForm();
   useEffect(() => {
     fetch(` http://localhost:5000/carPartsItems/${id}`)
       .then((res) => res.json())
       .then((data) => setPurchaseItems(data));
   }, [reload]);
+ /*  const handleChangeQuantity = event =>{
+    event.preventDefault();
+    const minQty=purchaseItems.quantity;
+    const userAddQty= event.target.value;
+    if(userAddQty<minQty){
+      //  SetQurantityError(event.target.value);
+      alert("Quantity min!!!");
+      
+    }
+  
+  } */
   const handleUpdateInventoryItem = (event) => {
     event.preventDefault();
     const quantity =
@@ -57,23 +72,13 @@ const PurchaseItem = () => {
         alert("CarPartsItems Quantity Delivered successfully!!!");
       });
   };
+  const onSubmit = async data => {
+    console.log(data)
+   // await createUserWithEmailAndPassword(data.email, data.password);
+   // await updateProfile({ displayName: data.name });
+   // console.log('update done');
+}
   return (
-    /* 
-        <div className="grid flex-grow card bg-base-300 rounded-box place-items-center">
-    <div className="card  bg-base-100 shadow-xl">
-      <figure className="px-10 pt-10">
-        <img style={{height:"350px"}} src={purchaseItems.img} alt="Shoes" className="rounded-xl" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title ">Product name: {purchaseItems.name}</h2>
-        <h1 className="text-xl font-semibold">Price: {purchaseItems.price}</h1>
-        <h1 className="text-xl font-semibold">Available_quantity: {purchaseItems.available_quantity}</h1>
-        <h1 className="text-xl font-semibold">Minimum_order_quantity: {purchaseItems.minimum_order_quantity}</h1>
-        <p><span className="font-semibold">Description:</span>{purchaseItems.description}</p>
-      </div>
-    </div> 
-    </div>  */
-
     <div className="grid grid-cols-1 md:grid-cols-2 mx-10 gap-5">
       <div className="card  bg-base-100 shadow-xl  mx-5">
         <figure className="px-10 pt-10">
@@ -108,52 +113,48 @@ const PurchaseItem = () => {
             <h2 className="text-center text-orange-800 text-2xl font-bold">
               Purchase
             </h2>
-            <form /* onSubmit={handleSubmit(onSubmit)} */>
+            <form  onSubmit={handleSubmit(onSubmit)} >
               <div className="form-control w-full max-w-xs">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
                 <input
                   type="text"
-                  placeholder="Your Name"
+                 
+                  disabled value={user?.displayName || ''}
                   className="input input-bordered input-primary w-full max-w-xs"
-                  /*   {...register("name", {
-                            required: {
-                                value: true,
-                                message: 'Name is Required'
-                            }
-                        })} */
+                  
+                 
                 />
                 <label className="label">
-                  {/*  {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>} */}
                 </label>
               </div>
-
               <div className="form-control w-full max-w-xs">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
-                  placeholder="Your Email"
+                
+                  disabled value={user?.email || ''}
                   className="input input-bordered input-primary w-full max-w-xs"
-                  /*  {...register("email", {
-                            required: {
-                                value: true,
-                                message: 'Email is Required'
-                            },
-                            pattern: {
-                                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                                message: 'Provide a valid Email'
-                            }
-                        })} */
                 />
-                <label className="label">
-                  {/*  {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>} */}
+                <label className="label">  
                 </label>
               </div>
-
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Product</span>
+                </label>
+                <input
+                  type="text"
+                
+                  disabled value={purchaseItems?.name || ''}
+                  className="input input-bordered input-primary w-full max-w-xs"
+                />
+                <label className="label">  
+                </label>
+              </div>
               <div className="form-control w-full max-w-xs">
                 <label className="label">
                   <span className="label-text">Quantity</span>
@@ -161,10 +162,30 @@ const PurchaseItem = () => {
                 <input
                   type="number"
                   name="quantity"
-                  placeholder="Total quantity"
+                  placeholder="The quantity you want"
                   className="input input-bordered input-primary w-full max-w-xs"
+                  {...register ("quantity", {
+                    required: {
+                        value: true,
+                        message: 'Provide a valid quantity required'
+                    },
+                    max: {
+                        value:`${purchaseItems.available_quantity}`,
+                        message: 'Less than available quantity required'
+                    },
+                    min: {
+                        value:`${purchaseItems.minimum_order_quantity}`,
+                        message: 'More than minimum quantity required'
+                    }
+                })} 
+                 
                 />
-                <label className="label"></label>
+               <label className="label">
+                               {errors.quantity?.type === 'required' && <span className="label-text-alt text-red-500">{errors.quantity.message}</span>} 
+                               {errors.quantity?.type === 'max' && <span className="label-text-alt text-red-500">{errors.quantity.message}</span>} 
+                               {errors.quantity?.type === 'min' && <span className="label-text-alt text-red-500">{errors.quantity.message}</span>} 
+
+                            </label>
               </div>
               <div className="form-control w-full max-w-xs">
                 <label className="label">
@@ -172,24 +193,14 @@ const PurchaseItem = () => {
                 </label>
                 <input
                   type="text"
+                  name="address"
                   placeholder="Your Address"
                   className="input input-bordered input-primary w-full max-w-xs"
-                  /* {...register("password", {
-                            required: {
-                                value: true,
-                                message: 'Password is Required'
-                            },
-                            minLength: {
-                                value: 6,
-                                message: 'Must be 6 characters or longer'
-                            }
-                        })} */
+                  
                 />
-
-                <label className="label">
-                  {/*  {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                        {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>} */}
-                </label>
+                 <label className="label">
+                
+            </label>
               </div>
               <div className="form-control w-full max-w-xs">
                 <label className="label">
@@ -203,7 +214,6 @@ const PurchaseItem = () => {
                 />
                 <label className="label"></label>
               </div>
-
               <input
                 className="btn btn-accent text-orange-800 font-bold  w-full max-w-xs text-white"
                 type="submit"
